456bb7de474af602a866d789e04792de82f8ec9b
mc-stack88 <mcstack88@gmail.com>
2021-06-21 19:34:16 +0530

    continued

--- src/components/Call.js
+++ src/components/Call.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// const CallStyle = styled.div`
+const ButtonStyle = styled.button`
+white-space: nowrap;
+display: inline-block;
+height: 40px;
+line-height: 40px;
+padding: 0 14px;
+background: #4460D9;
+border-radius: 4px;
+font-size: 14px;
+font-weight: normal;
+text-transform: uppercase;
+letter-spacing: 0.025em;
+color: #ffffff;
+text-decoration: none;
+-webkit-transition: all 0.15s ease;
+transition: all 0.15s ease;
+&:hover {
+  color: #ffffff;
+  background-color: lighten(#4460D9, 10%);
+  transform: translateY(-1px);
+  text-decoration: none;
+}
+`;
 
-// `;
 
 const Call = props => {
   const data = useStaticQuery(graphql`
@@ -18,7 +39,7 @@ const Call = props => {
     <div className="call">
       {props.showButton && (
         <div className="call-box-bottom">
-          <a href={data.contactJson.contact_button_link} className="button">Reserve Your Invite</a>
+          <ButtonStyle href={data.contactJson.contact_button_link}>Reserve Your Invite</ButtonStyle>
         </div>
       )}
     </div>
