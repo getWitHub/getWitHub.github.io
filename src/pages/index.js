3dd393ec177aaa5ae124e0728fa8b7f6dfeb125e
mcstack88 <mcstack88@pop-os.localdomain>
2021-06-26 19:01:35 +0530

    typeform embed

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import SEO from '../components/SEO';
 import Layout from '../components/Layout';
 import Call from '../components/Call';
 
-
 const Home = props => {
   const intro = props.data.intro;
   const site = props.data.site.siteMetadata;
@@ -82,7 +81,7 @@ const Home = props => {
           </div>
         </div>
       )}
-
+      <div class="typeform-widget" data-url="https://form.typeform.com/to/UGIXbK89?typeform-medium=embed-snippet" style={{width: '100%' , height: '50vh'}}></div> <script>{(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() }</script>
     </Layout>
   );
 };
