e72fcb3fd0b270562b3d6f7612227d5118e1f9cd
mcstack88 <mcstack88@pop-os.localdomain>
2021-06-26 19:31:39 +0530

    cta's scroll to bottom

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { graphql, useStaticQuery} from 'gatsby';
+import styled from 'styled-components';
+
+const LinkStyle = styled.a`
+  padding: 10px 12px 10px 12px;
+  display: inline-block;
+  font-weight: normal;
+  text-decoration: none;
+  color: #4460D9;
+  &.hover {
+    text-decoration: underline;
+  }
+  &.active {
+    font-weight: bold;
+    text-decoration: none;
+    &:hover {
+      text-decoration: none;
+      transition: all 225ms ease-in 0s;
+    }
+  }
+`;
 
 const Menu = props => {
   const data = useStaticQuery(graphql`
@@ -15,17 +35,28 @@ const Menu = props => {
       }
     }
   `);
+  const handleClick = () => {
+    window.scroll({
+      top: document.body.offsetHeight,
+      left: 0, 
+      behavior: 'smooth',
+    }); 
+  }
   return (
     <div id="main-menu" className="main-menu">
       <ul>
         {data.allMainMenuJson.edges.map(({ node }) => (
           <li key={node.name}>
-            <a className={node.name} href={node.url}>{node.name}</a>
+            <LinkStyle className={node.name} href={node.url}>{node.name}</LinkStyle>
           </li>
         ))}
+        <li>
+        <LinkStyle className="cta2" onClick={() => handleClick()}>Get Your Invite</LinkStyle>
+        </li>
       </ul>
     </div>
   );
 };
 
 export default Menu;
+
